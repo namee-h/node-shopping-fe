c966b5c3efbd42a4342ace5d54d79b49529a80be
Sara han <hans63237@gmail.com>
2025-08-03 02:43:00 +0900

    feat: 회원가입

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -20,7 +20,28 @@ export const registerUser = createAsyncThunk(
   async (
     { email, name, password, navigate },
     { dispatch, rejectWithValue }
-  ) => {}
+  ) => {
+    try {
+      const response = await api.post("/user", { email, name, password });
+
+      dispatch(
+        showToastMessage({
+          message: "회원가입을 축하드립니다 🎉",
+          status: "success",
+        })
+      );
+      navigate("/login");
+      return response.data.data;
+    } catch (error) {
+      dispatch(
+        showToastMessage({
+          status: "error",
+          message: "회원가입에 실패 했습니다",
+        })
+      );
+      return rejectWithValue(error.error);
+    }
+  }
 );
 
 export const loginWithToken = createAsyncThunk(
@@ -43,7 +64,19 @@ const userSlice = createSlice({
       state.registrationError = null;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(registerUser.fulfilled, (state) => {
+        state.loading = false;
+        state.registrationError = null;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.registrationError = action.payload;
+      });
+  },
 });
 export const { clearErrors } = userSlice.actions;
 export default userSlice.reducer;
